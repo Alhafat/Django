d664f1b2793b840baf79bc07ab5e8a326ca3b9cb
Alhafat <vladimir-lukin@inbox.ru>
2024-03-21 21:53:49 +0300

    hm_2 21.03.2024

--- hm/sem_02/management/commands/get_user.py
+++ hm/sem_02/management/commands/get_user.py
@@ -5,12 +5,10 @@ from sem_02.models import User
 class Command(BaseCommand):
     help = "Get user by id."
 
-
     def add_arguments(self, parser):
         parser.add_argument('id', type=int, help='User ID')
 
-
     def handle(self, *args, **kwargs):
-        id = kwargs['id']
-        user = User.objects.get(id=id)
+        pk = kwargs['id']
+        user = User.objects.get(id=pk)
         self.stdout.write(f'{user}')
